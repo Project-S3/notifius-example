984e59725f7485ff1e42524da494dde6d7062e8d
Jean-Philippe Lemay <info@jplemay.com>
2020-07-27 12:26:37 -0400

    okhttp encodage

--- src/main/java/NotifSenderExample.java
+++ src/main/java/NotifSenderExample.java
@@ -1,21 +1,13 @@
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import com.squareup.okhttp.*;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.nio.charset.StandardCharsets;
 
 public class NotifSenderExample
 {
     public static void main(String[] args) throws IOException, JSONException
     {
-        //Format de date
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-
         //Éléments notifications
         JSONObject notif = new JSONObject();
         notif.put("title", "Nouvelle note - Cours Base de données");
@@ -24,23 +16,14 @@ public class NotifSenderExample
 
         String cip = "lemj0601";
 
-        try {
-            System.out.println(Unirest.post("http://notifius.jplemay.com/users/lemj0601/notifications").header("Content-Type","application/json")
-                                      .body(notif.toString())
-                                      .asString());
-        } catch (UnirestException e) {
-            e.printStackTrace();
-        }
-
-//        OkHttpClient client = new OkHttpClient();
-//        MediaType mediaType = MediaType.parse("application/json");
-//        RequestBody body = RequestBody.create(mediaType,notif.toString());
-//        Request request = new Request.Builder()
-//                .url(String.format("http://notifius.jplemay.com/users/%s/notifications",cip))
-//                .method("POST", body)
-//                .build();
-//        Response response = client.newCall(request).execute();
-
-        //System.out.println(response.toString());
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType,new String(notif.toString().getBytes(System.getProperty("file.encoding")), StandardCharsets.UTF_8));
+        Request request = new Request.Builder()
+                .url(String.format("http://notifius.jplemay.com/users/%s/notifications",cip))
+                .method("POST", body)
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.toString());
     }
 }
