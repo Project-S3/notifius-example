9514616167c822e2c01b343186c8cf16053a3220
Jean-Philippe Lemay <jeanplemay@gmail.com>
2020-07-27 11:58:05 -0400

    unirest

--- src/main/java/NotifSenderExample.java
+++ src/main/java/NotifSenderExample.java
@@ -1,3 +1,6 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.squareup.okhttp.*;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,21 +19,28 @@ public class NotifSenderExample
         //Éléments notifications
         JSONObject notif = new JSONObject();
         notif.put("title", "Nouvelle note - Cours Base de données");
-        notif.put("content", "Vous avez une nouvelle note");
-        notif.put("date", dateFormat.format(new Date(System.currentTimeMillis())) );
+        notif.put("content", "Vous avez une nouvelle noteéàèàç");
         notif.put("service", "GRADE");
 
         String cip = "lemj0601";
 
-        OkHttpClient client = new OkHttpClient();
-        MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType,notif.toString());
-        Request request = new Request.Builder()
-                .url(String.format("http://notifius.jplemay.com/users/%s/notifications",cip))
-                .method("POST", body)
-                .build();
-        Response response = client.newCall(request).execute();
+        try {
+            System.out.println(Unirest.post("http://notifius.jplemay.com/users/lemj0601/notifications").header("Content-Type","application/json")
+                                      .body(notif.toString())
+                                      .asString());
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println(response.toString());
+//        OkHttpClient client = new OkHttpClient();
+//        MediaType mediaType = MediaType.parse("application/json");
+//        RequestBody body = RequestBody.create(mediaType,notif.toString());
+//        Request request = new Request.Builder()
+//                .url(String.format("http://notifius.jplemay.com/users/%s/notifications",cip))
+//                .method("POST", body)
+//                .build();
+//        Response response = client.newCall(request).execute();
+
+        //System.out.println(response.toString());
     }
 }
